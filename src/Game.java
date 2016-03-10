25ff45977a69827d091bbf16f5fbe6076ed03846
jcanderson195 <jcanderson195@gmail.com>
2016-03-10 15:47:38 -0600

    Added start menu that asks the user if they want to play single player mode (user vs computer) or two player mode (user vs user2)

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,10 @@ public class Game {
 	Userplayer Intro = new Userplayer();
 	Scanner keyboard = new Scanner( System.in );
 	
+	time OneSec = new time();
+	boolean run = true;
+	int choice = 0;
+	
 	public void getGame(){
 	
 		System.out.println("Welcome to the game of Nim!");
@@ -15,9 +19,46 @@ public class Game {
 		System.out.println("Here are the rules: 1. Choose who goes first. 2. You can pull either 1,2,or 3 counters from the pile of counters. Each player takes turns pulling counters from the pile. 3. The player to pull the last counter from the pile loses the game.");
 	
 		Intro.PlayerIntro();
+		
+		StartMenu();
 	
 	}
 	
+public void StartMenu(){
+		
+		
+		while (run){
+			OneSec.OneSecond();
+		System.out.println("Menu");
+		System.out.println("----");
+		OneSec.OneSecond();
+		System.out.println("1. Single Player");
+		System.out.println("2. Two Players");
+		System.out.println("3. Exit");
+		
+		choice = keyboard.nextInt();
+		
+		if (choice ==1){
+			
+			
+			break;
+		}else if(choice ==2){
+			
+			
+		}else if (choice == 3){
+			System.out.println("Thank You For Playing The Game of Nim!");
+			try {
+				Thread.sleep(50000);                 //1000 milliseconds is one second.
+			} catch(InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+			
+		}else {
+			System.out.println("Sorry we don't recognize that answer!");
+			
+		}
+		}
 	
 	
+}	
 }
